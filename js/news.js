1d0ff11be7eaddd336d67170247b725beb9d2481
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-01 03:27:58 +0600

    add another section

--- js/news.js
+++ js/news.js
@@ -6,12 +6,28 @@ const fetchNewsData = async () => {
 }
 
 const displayNewsData = news => {
+    // console.log(news);
     const newsCategory = document.getElementById('news__category');
     news.forEach(singleNews => {
-        console.log(singleNews.category_name);
         newsCategory.innerHTML += ` 
-        <a href=''>${singleNews.category_name}</a>
+        <p onclick="fetchCategoryData('${singleNews.category_id}','${singleNews.category_name}')" >${singleNews.category_name}</p>
         `
     })
 }
 fetchNewsData()
+
+
+const fetchCategoryData = async (id, name) => {
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    // console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategoryData(data.data, name);
+}
+
+const displayCategoryData = (categoryData, name) => {
+    // console.log(categoryData, name);
+    document.getElementById('category__items').innerHTML = categoryData.length;
+    document.getElementById('category__name').innerHTML = name;
+}
